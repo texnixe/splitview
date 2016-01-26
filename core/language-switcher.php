cf85f80ef04580e31b1394e9443a0810107aa6d2
Jens Törnell <jens.tornell@gmail.com>
2016-01-26 12:06:07 +0100

    Fix for language dir

--- core/language-switcher.php
+++ core/language-switcher.php
@@ -3,14 +3,15 @@ class splitviewLangaugeSwitcher {
 	public static $accepted = array('en', 'sv');
 	public static $default = 'en';
 	public static $current = '';
-	public static $dir = '/../languages/';
+	public static $dir = '';
 
 	public static function init() {
+		self::$dir = __DIR__ . DS . '..' . DS . 'languages';
 		self::set();
 		if( self::has() ) {
-			require_once self::$dir . self::$current . '.php';
+			require_once self::$dir . DS . self::$current . '.php';
 		} else {
-			require_once self::$dir . self::$default . '.php';
+			require_once self::$dir . DS . self::$default . '.php';
 		}
 	}
 
